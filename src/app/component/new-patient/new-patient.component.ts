2e468268ac1b2672a315c13afa5ebe0d345a8bcf
Miroslav Beronja <miroslav.beronja@qacube.com>
2021-02-27 10:22:45 +0100

    add now functionality on patient form

--- src/app/component/new-patient/new-patient.component.ts
+++ src/app/component/new-patient/new-patient.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormArray, FormGroup } from '@angular/forms';
-
+import {
+  FormBuilder,
+  Validators,
+  FormArray,
+  FormGroup,
+  AbstractControl,
+} from '@angular/forms';
 import * as moment from 'moment';
 import { AppService } from 'src/app/app.service';
 import { Observable, from } from 'rxjs';
@@ -8,6 +13,258 @@ import { startWith, map } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DialogInvalidComponent } from './dialog-invalid/dialog-invalid.component';
+
+@Component({
+  selector: 'app-new-patient',
+  templateUrl: './new-patient.component.html',
+  styleUrls: ['./new-patient.component.scss']
+})
+export class NewPatientComponent implements OnInit {
+
+  patientForm: FormGroup = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    date: ['', Validators.required],
+    codeVAT: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    doctorName: ['', Validators.required],
+    addresses: this.formBuilder.array([])
+  });
+
+  filteredOptions: Observable<any[]> = from([]);
+  dropdownValues: Array<{key: string, label: string}> = [];
+  doctors: Doctors[] = [];
+  submitted = false;
+  vatCodeRequired = true;
+  isDisabledSaveButton = false;
+
+  constructor(
+    private _snackBar: MatSnackBar,
+    private formBuilder: FormBuilder,
+    private appService: AppService,
+    public dialog: MatDialog,
+  ) {
+  }
+
+  ngOnInit(): void {
+
+    const dropdownValues = () =>
+      (Object.keys(DropdownValuesStatus).map((key: string) =>
+        new Invalid(key, DropdownValuesStatusLabels[key])
+      ));
+
+    this.dropdownValues = dropdownValues();
+    this.addresses.push(this.newAddressObject());
+
+    this.appService.getJSONDoctors()
+      .subscribe((doctors: Doctors[]) => {
+        this.doctors = doctors;
+        this.filteredOptions = this.patientForm.controls.doctorName.valueChanges
+          .pipe(
+            startWith(''),
+            map((value: any) => typeof value === 'string' ? value : value.firstName),
+            map((name: any) => name ? this._filter(name) : this.doctors.slice())
+          );
+    });
+
+    this.patientForm.controls.date.valueChanges.subscribe((date: Date) => {
+      const momentFormat = moment(date).format('YYYY-MM-DD');
+      const age = moment().diff(momentFormat, 'years', false);
+
+      this.patientForm.controls.codeVAT.clearValidators();
+      if (age >= 18) {
+        this.patientForm.controls.codeVAT.setValidators([Validators.required]);
+        this.vatCodeRequired = true;
+      } else {
+        this.vatCodeRequired = false;
+      }
+      this.patientForm.controls.codeVAT.updateValueAndValidity();
+    });
+  }
+
+  addNewAddress(extend: string) {
+    this.addresses.push(this.newAddressObject(extend));
+  }
+
+  newAddressObject(extend?: string): FormGroup {
+    const baseObj = {
+      phoneNumber: ['', [Validators.required, Validators.pattern(/^\+?[0-9\s]+$/)]],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      zipCode: ['', Validators.required],
+      country: ['', Validators.required]
+    }
+    const otherProps = {
+      state: ['', Validators.required],
+      name: [null]
+    }
+
+    let address
+    extend ? address = {...baseObj, ...otherProps} : address = baseObj;
+
+    return this.formBuilder.group(address);
+  }
+
+  displayFn(user: any): string {
+    return user && user.firstName ? user.firstName : '';
+  }
+
+  private _filter(name: string): any[] {
+    const filterValue = name.toLowerCase();
+
+    return this.doctors.filter((option: any) => option.firstName.toLowerCase().indexOf(filterValue) === 0);
+  }
+
+  get ptForm(): {[key: string]: any} | null { return this.patientForm.controls; }
+
+  get addresses() : FormArray { return this.patientForm.get('addresses') as FormArray; }
+
+  save(): void {
+    this.submitted = true;
+
+    if (this.patientForm.invalid) {
+
+      const invalidProps = this.foundInvalidField(this.patientForm.controls);
+      const dialogRef = this.dialog.open(DialogInvalidComponent, {
+        width: '650px',
+        data: invalidProps,
+      });
+
+      return;
+    }
+
+    this.patientForm.value.addresses.forEach((el: Addresses) => {
+      el.phoneNumber = el.phoneNumber.replace(/\s/g, '');
+    });
+
+    this.addresses.controls.forEach((el: AbstractControl) => {
+      let phoneNumber: AbstractControl | null = el.get('phoneNumber');
+
+      phoneNumber!.setValue(phoneNumber!.value.replace(/\s/g, ''));
+    });
+
+    this.appService.savePatient(this.patientForm.value)
+      .subscribe(data => {
+        this._snackBar.open('successfully', 'Close', {
+          duration: 2000,
+        });
+      })
+  }
+
+  foundInvalidField(controls: any) {
+
+    let listError: any = [];
+    Object.keys(controls).forEach((prop: any, propIndex: number) => {
+      const { errors } = controls[prop];
+
+      if (prop === 'addresses') {
+        const address = controls[prop];
+        address.controls.forEach((address: any, index: number) => {
+          let errorMessageAddress = `Address${index} - `;
+          if (Object.keys(address.controls).length) {
+            let counter = 0;
+            Object.keys(address.controls).forEach((propAddress: any) => {
+              const { errors: errorAddress } = address.controls[propAddress];
+              if (errorAddress) {
+                errorMessageAddress += this.getErrorMessage(errorAddress, counter, InvalidLabelsAddresses[propAddress]);
+                counter++;
+              }
+            });
+            if (address.status === 'INVALID') {
+              listError.push({label: errorMessageAddress});
+            }
+
+          }
+
+        });
+      } else {
+        if (errors) {
+          listError.push({label: this.getErrorMessage(errors, 0, InvalidLabels[prop])});
+        }
+      }
+    });
+    return listError;
+  }
+
+  getErrorMessage(errors: string, propIndex: number, nameOfprops: string) {
+    let errorMessage = '';
+
+    if (errors) {
+      const errorsExist  = (Object.keys(errors))[0];
+      if (errorsExist === 'pattern') {
+        !propIndex ?
+          errorMessage += `Number is not correct` :
+          errorMessage += `, Number is not correct`;
+      } else if (errorsExist === 'required') {
+        !propIndex ?
+          errorMessage += `${nameOfprops} is required` :
+          errorMessage += `, ${nameOfprops} is required`;
+      } else if (errorsExist === 'email') {
+        !propIndex ?
+          errorMessage += `Please enter a valid email address` :
+          errorMessage += `, Please enter a valid email address`;
+      }
+    }
+
+    return errorMessage;
+  }
+
+  removeAddress(index: number) {
+    this.addresses.removeAt(index);
+  }
+
+  change(state: string, index: number) {
+    const { WORK, CLOSE_RELATIVES } = DropdownValuesStatus;
+    const address: any = this.addresses.at(index);
+    const { name } = address['controls'];
+
+    name.setValue('');
+    if (state ==  WORK || state === CLOSE_RELATIVES) {
+      name.setValidators([Validators.required]);
+    } else {
+      name.clearValidators();
+    }
+
+    name.updateValueAndValidity();
+  }
+
+  onBlurMethod(index: number) {
+    const address: any = this.addresses.at(index);
+    const { phoneNumber } = address['controls'];
+    const { value } = phoneNumber;
+
+    if (String(value)[0] !== '+') {
+      phoneNumber.setValue(`+39${value}`);
+    }
+  }
+}
+
+export interface Patient {
+  date: Date,
+  firstName: string,
+  lastName: string,
+  doctorName: number,
+  addresses: Array<Addresses>
+  email: string;
+  codeVAT: string;
+}
+
+export interface Addresses {
+  phoneNumber: string,
+  street: string,
+  city: string,
+  zipcode: number,
+  country: string,
+  state?: string,
+  name?: string,
+}
+
+export interface Doctors {
+  firstName: string;
+  id: number;
+  lastName: string
+  title: string;
+}
 
 export enum DropdownValuesStatus {
   SECOND_HOME = 'SECOND_HOME',
@@ -50,188 +307,23 @@ export const InvalidLabels: any = {
   addresses: 'Addresses'
 };
 
-class Invalid {
-  key: any;
-  label: any;
+export const InvalidLabelsAddresses: any = {
+  phoneNumber: 'Phone number',
+  street: 'Street',
+  city: 'City',
+  zipCode: 'ZipCode',
+  country: 'Country',
+  state: 'State',
+  name: 'Name',
+};
 
-  constructor(key: any, label: string) {
+class Invalid {
+  key: string;
+  label: string;
+
+  constructor(key: string, label: string) {
     this.key = key;
     this.label = label;
-  }
-}
-
-@Component({
-  selector: 'app-new-patient',
-  templateUrl: './new-patient.component.html',
-  styleUrls: ['./new-patient.component.scss']
-})
-export class NewPatientComponent implements OnInit {
-
-  patientForm: any = this.formBuilder.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    date: ['', Validators.required],
-    codeVAT: ['', Validators.required],
-    email: ['', [Validators.email, Validators.required]],
-    doctorName: ['', Validators.required],
-    addresses: this.formBuilder.array([])
-  });
-
-  filteredOptions: Observable<any[]> = from([]);
-  dropdownValues: Array<{key: string, label: string}> = [];
-  doctors: any;
-  submitted = false;
-
-  constructor(
-    private _snackBar: MatSnackBar,
-    private formBuilder: FormBuilder,
-    private appService: AppService,
-    public dialog: MatDialog
-  ) {
-  }
-
-  ngOnInit(): void {
-
-    const dropdownValues = () =>
-      (Object.keys(DropdownValuesStatus).map((key: string) =>
-        new Invalid(key, DropdownValuesStatusLabels[key])
-      ));
-
-    this.dropdownValues = dropdownValues();
-    this.addresses.push(this.newAddressObject());
-
-    this.appService.getJSONDoctors()
-      .subscribe(doctors => {
-        this.doctors = doctors;
-        this.filteredOptions = this.patientForm.controls.doctorName.valueChanges
-          .pipe(
-            startWith(''),
-            map((value: any) => typeof value === 'string' ? value : value.firstName),
-            map((name: any) => name ? this._filter(name) : this.doctors.slice())
-          );
-    });
-
-    this.patientForm.controls.date.valueChanges.subscribe((date: any) => {
-      const momentFormat = moment(date).format('YYYY-MM-DD');
-      const age = moment().diff(momentFormat, 'years', false);
-
-      this.patientForm.controls.codeVAT.clearValidators();
-      if (age >= 18) {
-        this.patientForm.controls.codeVAT.setValidators([Validators.required]);
-      }
-      this.patientForm.controls.codeVAT.updateValueAndValidity();
-    });
-  }
-
-  addNewAddress(extend: string) {
-    this.addresses.push(this.newAddressObject(extend));
-  }
-
-  newAddressObject(extend?: string): FormGroup {
-    const baseObj = {
-      phoneNumber: ['', [Validators.required, Validators.pattern(/^\+?[0-9\s]+$/)]],
-      street: ['', Validators.required],
-      city: ['', Validators.required],
-      zipCode: ['', Validators.required],
-      country: ['', Validators.required]
-    }
-    const otherProps = {
-      state: ['', Validators.required],
-      name: [null,  Validators.required]
-    }
-
-    let address
-    extend ? address = {...baseObj, ...otherProps} : address = baseObj;
-
-    return this.formBuilder.group(address);
-  }
-
-  displayFn(user: any): string {
-    return user && user.firstName ? user.firstName : '';
-  }
-
-  private _filter(name: string): any[] {
-    const filterValue = name.toLowerCase();
-
-    return this.doctors.filter((option: any) => option.firstName.toLowerCase().indexOf(filterValue) === 0);
-  }
-
-  get ptForm(): any { return this.patientForm.controls; }
-
-  get addresses() : FormArray {
-    return this.patientForm.get('addresses') as FormArray;
-  }
-
-  save(): void {
-    this.submitted = true;
-
-    if (this.patientForm.invalid) {
-      const invalidProps = this.foundInvalidField(this.patientForm.controls);
-
-      const invalidData = () =>
-      (Object.keys(invalidProps).map((key: any) => {
-
-        return new Invalid(invalidProps[key], InvalidLabels[key]);
-      }));
-
-      const dialogRef = this.dialog.open(DialogInvalidComponent, {
-        width: '250px',
-        data: invalidData()
-      });
-
-      return;
-    }
-
-    this.patientForm.value.addresses.forEach((el: any) => {
-      el.phoneNumber = el.phoneNumber.replace(/\s/g, '');
-    });
-
-    this.appService.savePatient(this.patientForm.value)
-      .subscribe(data => {
-        this._snackBar.open('successfully', 'Close', {
-          duration: 2000,
-        });
-      })
-  }
-
-  foundInvalidField(controls: any) {
-
-    const invalid: any = {};
-    for (const name in controls) {
-      if (controls[name].invalid) {
-        invalid[name] = controls[name].invalid;
-      }
-    }
-    return invalid;
-  }
-
-  removeAddress(index: number) {
-    this.addresses.removeAt(index);
-  }
-
-  change(state: any, index: number) {
-    const { WORK, CLOSE_RELATIVES } = DropdownValuesStatus;
-    const address: any = this.addresses.at(index);
-    const { name } = address['controls'];
-
-    name.setValue('');
-    if (state ==  WORK || state === CLOSE_RELATIVES) {
-      name.setValidators([Validators.required]);
-    } else {
-      name.clearValidators();
-    }
-
-    name.updateValueAndValidity();
-  }
-
-  onBlurMethod(index: number) {
-    const address: any = this.addresses.at(index);
-    const { phoneNumber } = address['controls'];
-    const { value } = phoneNumber;
-
-    if (String(value)[0] !== '+') {
-      phoneNumber.setValue(`+39${value}`);
-    }
   }
 }
 
